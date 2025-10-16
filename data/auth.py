28c94a4c4118c4e3e596405b5e88fc6c282df585
yaroslav1171717 <yaroslav1171717@mail.ru>
2025-10-16 17:09:10 +0300

    Edit CI and auth

--- data/auth.py
+++ data/auth.py
@@ -1,14 +1,19 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()   # ищет файл .env в корневой директории вашего проекта и добавляет указанные там переменные среды в окружение программы
+if os.path.exists('.env'):
+    load_dotenv()
+  # ищет файл .env в корневой директории вашего проекта и добавляет указанные там переменные среды в окружение программы, если существует .env, т.к. в GIT его нет
 
 class Auth:
-
-    if os.environ['STAGE'] == 'release':
-        LOGIN = os.getenv("RELEASE_LOGIN")
-        PASSWORD = os.getenv("RELEASE_PASSWORD")
-    elif os.environ['STAGE'] == 'dev':
-        LOGIN = os.getenv("DEV_LOGIN")
-        PASSWORD = os.getenv("DEV_PASSWORD")
+    if os.path.exists('.env'):                # Здесь также проверяем, если есть .env, то от туда берем данные, а если нет, то данные будут браться из ->
+        if os.environ['STAGE'] == 'release':    # environment secret в GitHub, там для всех стендов переменные будут называться LOGIN и PASSWORD
+            LOGIN = os.getenv("RELEASE_LOGIN")
+            PASSWORD = os.getenv("RELEASE_PASSWORD")
+        elif os.environ['STAGE'] == 'dev':
+            LOGIN = os.getenv("DEV_LOGIN")
+            PASSWORD = os.getenv("DEV_PASSWORD")
+    else:
+        LOGIN = os.getenv("LOGIN")
+        PASSWORD = os.getenv("PASSWORD")
 
