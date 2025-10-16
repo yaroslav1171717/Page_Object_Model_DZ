329ae0a64b75a4c7632faf354f7b7953648f95b3
yaroslav1171717 <yaroslav1171717@mail.ru>
2025-10-16 17:50:10 +0300

    Edit auth and GIT 19

--- data/auth.py
+++ data/auth.py
@@ -3,20 +3,9 @@ from dotenv import load_dotenv
 
 if os.path.exists('.env'):
     load_dotenv()
-  # ищет файл .env в корневой директории вашего проекта и добавляет указанные там переменные среды в окружение программы, если существует .env, т.к. в GIT его нет
-
-# class Auth:
-#     if os.path.exists('.env'):                # Здесь также проверяем, если есть .env, то от туда берем данные, а если нет, то данные будут браться из ->
-#         if os.environ['STAGE'] == 'release':    # environment secret в GitHub, там для всех стендов переменные будут называться LOGIN и PASSWORD
-#             LOGIN = os.getenv("RELEASE_LOGIN")
-#             PASSWORD = os.getenv("RELEASE_PASSWORD")
-#         elif os.environ['STAGE'] == 'dev':
-#             LOGIN = os.getenv("DEV_LOGIN")
-#             PASSWORD = os.getenv("DEV_PASSWORD")
-#     else:
-#         LOGIN = os.getenv("LOGIN")
-#         PASSWORD = os.getenv("PASSWORD")
-
+# ищет файл .env в корневой директории нашего проекта и добавляет указанные там переменные среды в окружение программы, если существует .env, т.к. в GIT его нет
+# Далее также проверяем, если есть .env и конкретный стенд, то от туда берем данные, а если нет, то данные будут браться из ->
+# environment secret в GitHub, там для всех стендов переменные будут называться LOGIN и PASSWORD
 class Auth:
     def __init__(self):
         stage = os.environ.get('STAGE', '').strip().upper()
@@ -29,5 +18,4 @@ class Auth:
         else:
             self.LOGIN = os.getenv("LOGIN")
             self.PASSWORD = os.getenv("PASSWORD")
-        print(f"Stage: {os.environ.get('STAGE')}")
-        print(f"LOGIN: {self.LOGIN}, PASSWORD: {self.PASSWORD}")
+
