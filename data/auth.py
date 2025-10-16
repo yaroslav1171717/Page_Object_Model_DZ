b6a1e16eaf35c75e78dd3972303c0f4488df4832
yaroslav1171717 <yaroslav1171717@mail.ru>
2025-10-16 15:43:36 +0300

    Edits CI 5

--- data/auth.py
+++ data/auth.py
@@ -8,7 +8,7 @@ class Auth:
     if os.environ['STAGE'] == 'release':
         LOGIN = os.getenv("RELEASE_LOGIN")
         PASSWORD = os.getenv("RELEASE_PASSWORD")
-    elif os.environ['STAGE'] == 'qa':
-        LOGIN = os.getenv("QA_LOGIN")
-        PASSWORD = os.getenv("QA_PASSWORD")
+    elif os.environ['STAGE'] == 'dev':
+        LOGIN = os.getenv("DEV_LOGIN")
+        PASSWORD = os.getenv("DEV_PASSWORD")
 
